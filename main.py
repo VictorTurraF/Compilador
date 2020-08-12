aca04ad7186322f6b48678a4cdcc144dc34c0baa
VictorTurraF <victor.florencio@univem.edu.br>
2020-08-11 22:25:35 -0300

    Tabelas de tokens adicionadas

---
+++ main.py
@@ -0,0 +1,13 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+print("Hello world")
+PalavrasReservadas = ["asm", "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"]
+
+Letras = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+Digitos = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+SimbolosEspeciais = [".", ",", ";", "(", ")", ":", "+", "-", "/", "*", "%", "<", ">","{", "}","[","]", "="]
+
+SimbolosCompostos = ["++", "--", ">=", "<=", "+=", "-=", "*=", "/=", "%=", "=="]
