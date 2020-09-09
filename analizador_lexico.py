a63d09231e1e618e72c6f1dea7cc6b9e41c9b419
VictorTurraF <victor.florencio@univem.edu.br>
2020-09-08 22:42:08 -0300

    Adicionado novo módulo: Analisador Léxico

---
+++ analizador_lexico.py
@@ -0,0 +1,165 @@
+import re
+
+# Analizador Lexico
+
+class Tokenizador:
+
+  ## Propriedades ##
+
+  # Const Palavras reservadas da linguagem
+  __palavras_reservadas = [
+    "asm", "auto", "break", 
+    "case", "char", "const", 
+    "continue", "default", "do", 
+    "double", "else", "enum", 
+    "extern", "float", "for", 
+    "goto", "if", "int", 
+    "long", "main", "register", 
+    "return", "short", "signed", 
+    "sizeof", "static", "struct", 
+    "switch", "typedef", "union", 
+    "unsigned", "void", "volatile", 
+    "while",
+  ]
+
+  # Const. Simbolos especiais da linguagem
+  __simbolos_especiais = [
+    ".", ",", ";", 
+    "(", ")", ":", 
+    "+", "-", "/", 
+    "*", "%", "<", 
+    ">", "{", "}", 
+    "[", "]", "=",
+  ]
+
+  # Const. Operadores compostos da linguagem
+  __simbolos_compostos = [
+    "++", "--", ">=", 
+    "<=", "+=", "-=", 
+    "*=", "/=", "%=", 
+    "==",
+  ]
+
+  # tabela do resultado
+  __lista_tokens = []
+
+  # Buffer para armazenar constantes de Strings
+  __buffer = ""
+
+  # Buffer Flag, para indicar inicio e encerramento do buffer
+  __buffer_flag = False
+
+  # Código
+  __codigo = ""
+
+  # Array de linhas do codigo
+  __linhas = []
+
+
+
+
+  ## Metodos da Classe ##
+
+  # Remove espaços em branco desnecessario 
+  # ( dois ou mais espaços são convertidos para um espaço apenas )
+  def __remover_espacos(self, cod):
+    return re.sub("[ \t\r\f\v]{2,}", " ", cod)
+
+  # Retorna um array com as linhas do codigo
+  def __quebrar_linhas(self, cod):
+    return re.split("\n", cod )
+
+  # Retorna um array com os tokens
+  def __quebrar_simbolos( self, linha ):
+    return re.split("(\+\+|--|<=|>=|==|\+=|-=|\*=|/=|%=|\W)", linha)
+
+  # Encontra diretivas de compilados
+  def __buscar_diretivas( self, linha ):
+    return re.search( "^(//|#)(.*)", linha )
+
+  def __gerar_tokens(self):
+
+    # Remove os espaços desnecessários
+    self.__codigo = self.__remover_espacos( self.__codigo )
+
+    # Quebra o codigo em linhas
+    self.__linhas = self.__quebrar_linhas( self.__codigo )
+
+    # percorre as linhas
+    for i, linha in enumerate( self.__linhas ) :
+
+      # Busca por diretivas para ignorar a analise
+      diretivas = self.__buscar_diretivas( linha )
+
+      # se existir alguma diretiva
+      if diretivas:
+        token = diretivas.group(1) 
+        desc_simbolo = "Diretiva: Ignore a linha"
+        print( i + 1, "\t", token, "\t\t", desc_simbolo )
+        continue
+      
+      # Quebra a linha em espaços e simbolos e simbolos compostos
+      tokens = self.__quebrar_simbolos( linha ) 
+
+      #percorre os tokens
+      for token in tokens:
+
+        desc_simbolo = ""
+   
+        # Verifica a existencia de cadeias
+        if self.__buffer_flag :
+          if token == '"' :
+            print( i + 1, "\t", self.__buffer, "\t\t", "String Constante" )
+            self.__buffer_flag = False
+            self.__buffer = ""
+            desc_simbolo = "Delimitador de String"
+          else :
+            self.__buffer += token
+            continue
+        else :
+          if token == '"' : # Caso seja inicio de string
+            self.__buffer_flag = True # inicia o buffer
+
+        # Converte o token para minusculas
+        token = token.lower()
+
+        # Verifica os tokens
+        if token == "" or token == " " :
+          continue
+        
+        elif token == "\"":
+          desc_simbolo = "Delimitador de String"
+
+        elif token in self.__simbolos_especiais:
+          desc_simbolo = "Simbolo especial"
+
+        elif token in self.__simbolos_compostos:
+          desc_simbolo = "Simbolo Composto"
+
+        elif token in self.__palavras_reservadas:
+          desc_simbolo = "Palavra Reservada"
+    
+        elif token.isdigit() :
+          desc_simbolo = "Contante inteira"
+
+        else :
+          desc_simbolo = "Identificador"
+
+        print( i + 1, "\t", token, "\t\t", desc_simbolo )
+
+        self.__lista_tokens.append( [ i, token, desc_simbolo ] )
+
+
+
+
+  ## Metodos publicos ##
+
+  def getTokens( self, codigo ):
+
+    # atribuição do código
+    self.__codigo = codigo
+
+    # geração dos tokens
+    self.__gerar_tokens()
+
+    return self.__lista_tokens
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-import re
 
-PalavrasReservadas = ["asm", "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float", "for", "goto", "if", "int", "long", "main", "register", "return", "short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"]
-SimbolosEspeciais = [".", ",", ";", "(", ")", ":", "+", "-", "/", "*", "%", "<", ">", "{", "}", "[", "]", "="]
-SimbolosCompostos = ["++", "--", ">=", "<=", "+=", "-=", "*=", "/=", "%=", "=="]
+# classe do analizador lexico
+from analizador_lexico import Tokenizador
 
+# codigo fonte de entrada
 codigo = """#include <stdio.h>
 main()
 { 
@@ -16,81 +15,7 @@ main()
   printf("Valores a = %d e b = %d", a, b);
 }"""
 
-# tabela do resultado
-tabela_analise = []
+tokenizador = Tokenizador()
+tokens = tokenizador.getTokens( codigo )
 
-# Buffer para armazenar constantes de Strings
-buffer = ""
-
-# Buffer Flag, para indicar inicio e encerramento do buffer
-buffer_flag = False
-
-# Variavel Descrição do simbolo
-simbolo = ""
-
-# Remove espaços em branco desnecessario 
-# ( dois ou mais espaços são convertidos para um espaço apenas )
-codigo = re.sub("[ \t\r\f\v]{2,}", ' ', codigo)
-
-# Quebra o codigo em linhas
-result = re.split("\n", codigo )
-
-# Para cada linha do codigo
-for linha in range( len( result ) ):
-  
-  # Verifica se contem diretivas para ignorar a analise
-  ignore = re.search( "^(//|#)(.*)", result[linha] )
-  if ignore:
-    token = ignore.group(1) 
-    simbolo = "Diretiva: Ignore a linha"
-    print( "\t", linha + 1, "\t", token, "\t\t", simbolo )
-    continue
-
-  # Quebra a linha em espaços e simbolos e simbolos compostos
-  tokens = re.split("(\+\+|--|<=|>=|==|\+=|-=|\*=|/=|%=|\W)", result[linha])
-  
-  # print( tokens )
-  for token in tokens:
-
-    simbolo = ''
-
-    if buffer_flag :
-      if token == '"' :
-        print( "\t", linha + 1, "\t", buffer, "\t\t", "String Constante" )
-        buffer_flag = False
-        buffer = ''
-        simbolo = "Delimitador de String"
-      else :
-        buffer += token
-        continue
-    else :
-      if token == '"' : # Caso seja inicio de string
-        buffer_flag = True # inicia o buffer
-        simbolo = "Delimitador de String"
-    
-    # Converte o token para minusculas
-    token = token.lower()
-
-    if token == '' or token == ' ' :
-      continue
-
-    elif token in SimbolosEspeciais:
-      simbolo = "Simbolo especial"
-
-    elif token in SimbolosCompostos:
-      simbolo = "Simbolo Composto"
-
-    elif token in PalavrasReservadas:
-      simbolo = "Palavra Reservada"
- 
-    elif token.isdigit() :
-      simbolo = "Contante inteira"
-
-    else :
-      simbolo = "Identificador"
-
-    tabela_analise.append( [ linha, token, simbolo ] )
-    print( "\t", linha + 1, "\t", token, "\t\t", simbolo )
-
-
-print(tabela_analise)
+print(tokens)
